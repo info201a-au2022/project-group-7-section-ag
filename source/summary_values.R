6bbf4b2ea50505811c63a5fbe03ba74061bbf7d6
KelliAnn Ramirez <kelliannramirez@KelliAnns-MacBook-Air.local>
2022-11-13 14:19:11 -0800

    added summary table to index.Rmd

--- source/summary_values.R
+++ source/summary_values.R
@@ -21,7 +21,7 @@ monthly_avg_change <- temp_change_country %>%
   summarise(avg_change_total = mean(avg_change))
 
 # 2b - month of the highest temp change (VALUE 1: highest_month_val)
-highest_val <- max(monthly_avg_change$avg_change_total)
+highest_val <- round(max(monthly_avg_change$avg_change_total), digits=2)
 
 highest_month <- monthly_avg_change %>% 
   filter_all(any_vars(. %in% c(highest_val))) %>% 
@@ -30,7 +30,7 @@ highest_month <- monthly_avg_change %>%
 highest_month_val <- paste0(highest_month, ": ", highest_val)
 
 # 2c - month of the lowest temp change (VALUE 2: lowest_month_val)
-lowest_val <- min(monthly_avg_change$avg_change_total)
+lowest_val <- round(min(monthly_avg_change$avg_change_total), digits=2)
 
 lowest_month <- monthly_avg_change %>% 
   filter_all(any_vars(. %in% c(lowest_val))) %>% 
@@ -56,6 +56,23 @@ avg_oceania_temp_change <- oceania_temp_change %>%
 
 # exoplanets.csv Summary Values
 exoplanets <- read_csv('../data/exoplanets.csv')
+
+# select columns that we might care about
+exoplanets <- exoplanets %>% select(pl_name, hostname, sy_snum, sy_pnum, 
+                                    discoverymethod, disc_year, pl_orbper, 
+                                    pl_orbsmax, pl_rade, pl_radj, pl_bmasse, 
+                                    pl_bmassj, pl_orbeccen, pl_eqt, st_spectype,
+                                    st_teff, st_rad, st_mass, st_logg,
+)
+
+# rename columns to more understandable names
+colnames(exoplanets) <- c("planet_name", "host_name", "num_stars", "num_planets",
+                          "discovery_method", "discovery_year", "orbital_period_days",
+                          "orbital_semi_maj_axis_au", "planet_rad_e", "planet_rad_j",
+                          "planet_mass_e", "planet_mass_j", "eccentricity", "planet_equi_temp_k",
+                          "spectral_type", "stellar_eff_temp_k", "stellar_rad_sol", "stellar_mass_sol",
+                          "stellar_surf_grav"
+)
 # 1 - 
 # make dataset smaller so that there is one row per planet, should have 5044 rows
 planet_summary <- exoplanets %>% 
@@ -109,8 +126,8 @@ colnames(num_planets_smaller_earth) = c("value", "count")
 
 # VALUE #???: percent of planets bigger and smaller than Earth
 # percent_bigger, percent_smaller
-percent_bigger <- (num_planets_smaller_earth[1, 2] / sum(num_planets_smaller_earth$count)) * 100
-percent_smaller <- (num_planets_smaller_earth[2, 2] / sum(num_planets_smaller_earth$count)) * 100
+percent_bigger <- round((num_planets_smaller_earth[1, 2] / sum(num_planets_smaller_earth$count)) * 100, digits=2)
+percent_smaller <- round((num_planets_smaller_earth[2, 2] / sum(num_planets_smaller_earth$count)) * 100, digits=2)
 
 # Value #???: percent chance we'll arrive at a habitable exoplanet out of the 5044 in the dataset
 # percent_habitable
@@ -118,101 +135,119 @@ num_habitable <- planet_summary %>%
   filter(planet_equi_temp_k >= 273 & planet_equi_temp_k <= 300) %>% 
   nrow()
 
-percent_habitable <- (num_habitable / nrow(planet_summary)) * 100
+percent_habitable <- round((num_habitable / nrow(planet_summary)) * 100, digits=2)
 
 
 # fires.csv Summary Values
 file = "https://raw.githubusercontent.com/info201a-au2022/project-group-7-section-ag/main/data/fires.csv"
 
-fires <- read_csv(url(file))
+# fires <- read_csv(url(file))
+# 
+# #1 Fires per year
+# year_df <- fires %>%
+#   group_by(year) %>%
+#   summarize(count = n())
+# 
+# #2 Fires in each state in 1992
+# state_1992_df <- fires %>%
+#   filter(year == "1992") %>%
+#   group_by(state) %>%
+#   summarize(count = n())
+# 
+# #3 Fires in each state in 1998
+# state_1998_df <- fires %>%
+#   filter(year == "1998") %>%
+#   group_by(state) %>%
+#   summarize(count = n())
+# 
+# #4 Fires in each state in 2015
+# state_2015_df <- fires %>%
+#   filter(year == "2015") %>%
+#   group_by(state) %>%
+#   summarize(count = n())
+# 
+# #5a Year with max fires
+# max_year <- year_df %>%
+#   filter(count == max(count)) %>%
+#   pull(year)
+# #2015
+# 
+# #5b Year with min fires
+# min_year <- year_df %>%
+#   filter(count == min(count)) %>%
+#   pull(year)
+# #1998
+# 
+# #6a Max fires in a year (2015)
+# count_max_year <- year_df %>%
+#   filter(count == max(count)) %>%
+#   pull(count)
+# # 46409
+# 
+# #6b Min fires in a year (1998)
+# count_min_year <- year_df %>%
+#   filter(count == min(count)) %>%
+#   pull(count)
+# # 9363
+# 
+# #7 Fires in each state
+# state_df <- fires %>%
+#   group_by(state) %>%
+#   summarize(count = n())
+# 
+# #8a State with max fires
+# max_state <- state_df %>%
+#   filter(count == max(count)) %>%
+#   pull(state)
+# # GA
+# 
+# #8b State with min fires
+# min_state <- state_df %>%
+#   filter(count == min(count)) %>%
+#   pull(state)
+# # DE HI
+# 
+# #9 Fires per month
+# month_df <- fires %>%
+#   group_by(month) %>%
+#   summarize(count = n())
+# 
+# #10a Month with max fires
+# max_month <- month_df %>%
+#   filter(count == max(count)) %>%
+#   pull(month)
+# # 07 July
+# 
+# #10b Month with min fires
+# min_month <- month_df %>%
+#   filter(count == min(count)) %>%
+#   pull(month)
+# # 12 December
+# 
+# #11 Fires in earliest year recorded (1992)
+# count_1992 <- year_df %>%
+#   filter(year == "1992") %>%
+#   pull(count)
+# # 15598
+# 
+# #12 Difference in fires from 1992 to 2015
+# fire_diff <- count_max_year - count_1992
+# # 30811
 
-#1 Fires per year
-year_df <- fires %>%
-  group_by(year) %>%
-  summarize(count = n())
+summary_values_df <- data.frame(highest_month_val,
+                                lowest_month_val,
+                                percent_bigger,
+                                percent_smaller,
+                                percent_habitable
+                                )
 
-#2 Fires in each state in 1992
-state_1992_df <- fires %>%
-  filter(year == "1992") %>%
-  group_by(state) %>%
-  summarize(count = n())
+# strings
+highest_month_val
+lowest_month_val
 
-#3 Fires in each state in 1998
-state_1998_df <- fires %>%
-  filter(year == "1998") %>%
-  group_by(state) %>%
-  summarize(count = n())
+# numerics
+percent_bigger
+percent_smaller
+percent_habitable
 
-#4 Fires in each state in 2015
-state_2015_df <- fires %>%
-  filter(year == "2015") %>%
-  group_by(state) %>%
-  summarize(count = n())
 
-#5a Year with max fires
-max_year <- year_df %>%
-  filter(count == max(count)) %>%
-  pull(year)
-#2015
-
-#5b Year with min fires
-min_year <- year_df %>%
-  filter(count == min(count)) %>%
-  pull(year)
-#1998
-
-#6a Max fires in a year (2015)
-count_max_year <- year_df %>%
-  filter(count == max(count)) %>%
-  pull(count)
-# 46409
-
-#6b Min fires in a year (1998)
-count_min_year <- year_df %>%
-  filter(count == min(count)) %>%
-  pull(count)
-# 9363
-
-#7 Fires in each state
-state_df <- fires %>%
-  group_by(state) %>%
-  summarize(count = n())
-
-#8a State with max fires
-max_state <- state_df %>%
-  filter(count == max(count)) %>%
-  pull(state)
-# GA
-
-#8b State with min fires
-min_state <- state_df %>%
-  filter(count == min(count)) %>%
-  pull(state)
-# DE HI
-
-#9 Fires per month
-month_df <- fires %>%
-  group_by(month) %>%
-  summarize(count = n())
-
-#10a Month with max fires
-max_month <- month_df %>%
-  filter(count == max(count)) %>%
-  pull(month)
-# 07 July
-
-#10b Month with min fires
-min_month <- month_df %>%
-  filter(count == min(count)) %>%
-  pull(month)
-# 12 December
-
-#11 Fires in earliest year recorded (1992)
-count_1992 <- year_df %>%
-  filter(year == "1992") %>%
-  pull(count)
-# 15598
-
-#12 Difference in fires from 1992 to 2015
-fire_diff <- count_max_year - count_1992
-# 30811
