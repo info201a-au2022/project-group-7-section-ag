2bf5160fbff41977e267335e19159af5600cb6c8
KelliAnn Ramirez <kelliannramirez@KelliAnns-MacBook-Air.local>
2022-11-14 11:19:04 -0800

    fixed summary table, index.Rmd formatting

--- source/summary_values_table.R
+++ source/summary_values_table.R
@@ -1,4 +1,4 @@
-source("summary_values.R")
+source("../source/summary_values.R")
 
 summary_values_df <- data.frame(highest_month_val,
                                 lowest_month_val,
@@ -9,13 +9,13 @@ summary_values_df <- data.frame(highest_month_val,
                                 percent_diff
                                 )
 
-# strings
-highest_month_val
-lowest_month_val
-
-# numerics
-percent_bigger
-percent_smaller
-percent_habitable
-fire_diff
-percent_diff
+# # strings
+# highest_month_val
+# lowest_month_val
+# 
+# # numerics
+# percent_bigger
+# percent_smaller
+# percent_habitable
+# fire_diff
+# percent_diff
